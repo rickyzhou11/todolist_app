971a24434c421e188caee38473f1b841ef34418d
rickyzhou11 <rickyzhou@utexas.edu>
2014-04-14 19:36:53 +0000

    new commit to try to get rid of the directory error

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodolistApp::Application.config.secret_key_base = 'e9e3e45628bd399f17f9e850153d1e18a1c421a0184bd7fcc93c67a9921e93bbe02c670915b1e254e81055f22cd22c9576b46399531e97184e284502b28a98a3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TodolistApp::Application.config.session_store :cookie_store, key: '_todolist_app_session'
